1692656cf2b2d5e747d37a4e7855c15379bb12c6
Andrea Lin <andrea.lin3110@gmail.com>
2025-03-22 22:28:57 +0100

    Set `Roboto` as default font

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 const App = () => {
   return (
-    <>
+    <main className="w-screen h-screen grid place-items-center">
       <p className="text-4xl">Hello World!</p>
-    </>
+    </main>
   )
 }
 
