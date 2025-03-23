86fc56866b5fd8b2bfe248cb6077dda086f637a5
Andrea Lin <andrea.lin3110@gmail.com>
2025-03-23 02:39:00 +0100

    Add `vite-plugin-singlefile`
    - This avoids CORS errors due to the use of `type="module"` in the script tags.

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    viteSingleFile(),
     tailwindcss(),
     tsConfigPaths(),
     viteStaticCopy({
