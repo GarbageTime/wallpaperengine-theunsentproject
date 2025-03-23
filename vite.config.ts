87e294ea0c97a800dfc0cc8d201f94013e271bb4
Andrea Lin <andrea.lin3110@gmail.com>
2025-03-23 01:44:01 +0100

    Add Steam Community Files to Build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +10,13 @@ export default defineConfig({
     react(),
     tailwindcss(),
     tsConfigPaths(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'assets/steam/**/*',
+          dest: '.'
+        }
+      ]
+    })
   ],
 })
